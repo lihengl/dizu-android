e5032e8f6a34e5f3a4813f6e0a2d6638d9d123cb
Li-Heng Liang <leonlhliang@gmail.com>
2014-11-23 19:50:44 +0800

    Tests are runnable now

--- tests/src/com/lihengl/dizu/MainActivityTest.java
+++ tests/src/com/lihengl/dizu/MainActivityTest.java
@@ -2,20 +2,15 @@ package com.lihengl.dizu;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-/**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class com.lihengl.dizu.MainActivityTest \
- * com.lihengl.dizu.tests/android.test.InstrumentationTestRunner
- */
+
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public MainActivityTest() {
         super("com.lihengl.dizu", MainActivity.class);
+    }
+
+    public void testPreConditions() {
+        assertTrue(true);
     }
 
 }
