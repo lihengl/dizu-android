d14f4c2db8cfd2938220457947ad669d970dc36a
Li-Heng Liang <leonlhliang@gmail.com>
2014-11-25 23:43:50 +0800

    Familiarize with testing framework

--- tests/src/com/lihengl/dizu/MainActivityTest.java
+++ tests/src/com/lihengl/dizu/MainActivityTest.java
@@ -7,16 +7,28 @@ import android.test.ActivityInstrumentationTestCase2;
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
     private static final String TAG = "MainActivityTest";
 
+    private MainActivity sbjActivity;
+
     public MainActivityTest() {
         super("com.lihengl.dizu", MainActivity.class);
     }
 
-    public void setUp() {
-        Log.d(TAG, "Hello?");
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        Log.d(TAG, "Setup");
+        sbjActivity = getActivity();
     }
 
     public void testPreConditions() {
+        Log.d(TAG, "Precondition");
         assertTrue(true);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        Log.d(TAG, "Teardown!");
+        super.tearDown();
     }
 
 }
